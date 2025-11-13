6dd1e6434573af5d6ae7a1e4cfb00101f0fa4a76
wevergton00 <wevergton.sousa@escola.pr.gov.br>
2025-11-13 11:07:40 -0300

    cript.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Rolagem suave nos links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
