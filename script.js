982842a9e550def25f5ae83bcb3d1416fd319192
wevergton00 <wevergton.sousa@escola.pr.gov.br>
2025-11-17 11:24:33 -0300

    Up07

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ document.querySelectorAll('a').forEach(anchor => {
     });
 });
 
+// Adicionando rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
